3afd6846611d63306dcd13819801aebf4b561d3c
schade <gerald.schade@gmx.de>
2019-07-02 02:03:24 +0200

    Version 0.10225

---
+++ aufruf.sh
@@ -0,0 +1,1 @@
+make debug install git && G_MESSAGES_DEBUG=all gdb -ex "b appobject.c:233" -ex "b appobject.c:228" -ex "b appobject.c:83" -ex "b gemessages.c:1341" -ex "catch signal SIGSEGV" --args fbfax -v -dt /DATA/down/billing_19034615_4373c02ac8fc2.pdf -an 616381
